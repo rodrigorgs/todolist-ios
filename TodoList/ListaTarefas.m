f68c8f319e94ed1a5d9f4fe79931bf72da79b74d
rodrigorgs <rodrigorgs@gmail.com>
2014-01-24 16:55:42 -0300

    Implementa persistencia.

--- TodoList/ListaTarefas.m
+++ TodoList/ListaTarefas.m
@@ -18,16 +18,29 @@
 {
     self = [super init];
     if (self) {
-        self.tarefas = [[NSMutableArray alloc] init];
+        NSUserDefaults *prefs =[NSUserDefaults standardUserDefaults];
+        
+        NSArray *array = [prefs objectForKey:@"tarefas"];
+        if (!array)
+            array = @[];
+        self.tarefas = [NSMutableArray arrayWithArray:array];
     }
     return self;
 }
 
+- (void)salva {
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    [prefs setObject:self.tarefas forKey:@"tarefas"];
+    [prefs synchronize];
+}
+
 - (void)adicionaItem:(NSString *)item {
     [self.tarefas addObject:item];
+    [self salva];
 }
 - (void)limpaItens {
     [self.tarefas removeAllObjects];
+    [self salva];
 }
 - (NSString *)description {
     return [self.tarefas componentsJoinedByString:@"\n"];
